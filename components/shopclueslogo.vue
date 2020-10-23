5bab7af27fedc9b06569ba53be3144806093d27e
riyakhokhani <riyakhokhani140@gmail.com>
2020-10-23 23:36:25 +0530

    ICA 2

---
+++ components/shopclueslogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <img
+    class="shopclueslogo"
+    alt="shopclueslogo"
+    src="shopclueslogo.png"
+  >
+</template>
+
+<style>
+.shopclueslogo {
+  height: 50px;
+  width: 50px;
+}
+</style>
